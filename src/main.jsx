899a4685519e1ec230bc85f06dede3ec8073f448
MiguelAngelTG7 <michael_angello@hotmail.com>
2025-09-17 20:54:35 -0500

    install analytics vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+    <Analytics />
+  </React.StrictMode>
+);
