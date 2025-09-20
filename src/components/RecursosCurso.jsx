2bb8e739b343fd6a4bea9e193f073abeef9d06c9
MiguelAngelTG7 <michael_angello@hotmail.com>
2025-09-19 22:10:01 -0500

    Todo ok ok ok

--- src/components/RecursosCurso.jsx
+++ src/components/RecursosCurso.jsx
@@ -46,7 +46,9 @@ const RecursosCurso = ({ claseId, esProfesor }) => {
   return (
     <div className="mt-4">
       <hr style={{ margin: '24px 0 16px 0' }} />
-      <h4 style={{ fontSize: '0.9rem', textAlign: 'center', marginBottom: '18px' }}>Recursos del Curso</h4>
+      <h5 style={{ fontSize: '1rem', fontWeight: 'bold', textAlign: 'center', marginBottom: '18px' }}>
+        Recursos del Curso
+      </h5>
       {esProfesor && (
         <form className="mb-3 d-flex gap-2" onSubmit={handleAgregar}>
           <input
